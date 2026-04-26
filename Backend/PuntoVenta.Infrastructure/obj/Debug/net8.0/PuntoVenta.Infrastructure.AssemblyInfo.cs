659071212b269c2e1b11970e4093e66da4c2f271
LuisFGom <luisgomezllerena2.0@gmail.com>
2026-04-26 17:11:27 -0500

    TODO BIEN (BACK Y MAIL) FALTA FRONT

--- Backend/PuntoVenta.Infrastructure/obj/Debug/net8.0/PuntoVenta.Infrastructure.AssemblyInfo.cs
+++ Backend/PuntoVenta.Infrastructure/obj/Debug/net8.0/PuntoVenta.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PuntoVenta.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e855e48d7ed139b43bfc6a7c567ca113b422da34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae673498a0aeb48a42137d4405361091336deb4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PuntoVenta.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PuntoVenta.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
